5cf9ea44381a481f5955d946adbffc556b3d0a8c
Pankrat <panknotdeadd@gmail.com>
2026-03-15 16:50:00 +0300

    register Room in admin and clean up model

--- src/rooms/models.py
+++ src/rooms/models.py
@@ -2,15 +2,16 @@ from django.db import models
 
 
 class Room(models.Model):
+    """
+    Модель таблицы для комнат
+    """
+
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ["pk"]  # сортировка по умолчанию по id
-        indexes = [
-            models.Index(fields=["id"]),  # индекс для ускорения запросов по цене
-        ]
 
     def __str__(self):
         return f"Room {self.pk}: {self.description} - ${self.price}"
