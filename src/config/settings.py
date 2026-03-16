1b568ae9a55acfe5539f6aebce5b3f69b86e31ae
Pankrat <panknotdeadd@gmail.com>
2026-03-17 00:47:31 +0300

    move Django settings to environment variables

--- src/config/settings.py
+++ src/config/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5girduuoej2rz6^=5b06_p$f@8@sqq^(i5ahlx=7_nr1-6y46p"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = []  # type: ignore
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS", default="", cast=lambda v: [s.strip() for s in v.split(",") if s.strip()]
+)
 
 # Application definition
 
