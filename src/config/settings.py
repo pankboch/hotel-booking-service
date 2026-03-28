484bc019fed1a27a2c58a3c9fe69e5c0e02f35eb
Pankrat <panknotdeadd@gmail.com>
2026-03-28 18:14:20 +0300

    fix project settings for docker environment

--- src/config/settings.py
+++ src/config/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django_extensions",
     "rooms.apps.RoomsConfig",
     "bookings.apps.BookingsConfig",
     "rest_framework",
@@ -111,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "ru"
 
-TIME_ZONE = "europe/moscow"
+TIME_ZONE = "Europe/Moscow"
 
 USE_I18N = True
 
